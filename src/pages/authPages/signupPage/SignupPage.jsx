bc06124e672d50b9f8957873b275ae6a22065d63
dongyoon7212 <dongyoon7212@naver.com>
2024-06-09 22:33:52 +0900

    Feat: 회원가입 페이지 레이아웃 구현

--- src/pages/authPages/signupPage/SignupPage.jsx
+++ src/pages/authPages/signupPage/SignupPage.jsx
@@ -1,34 +1,35 @@
 /** @jsxImportSource @emotion/react */
-import { useNavigate } from "react-router-dom";
 import * as s from "./style";
 
-function SigninPage() {
-    const navigate = useNavigate();
-    const handleSignupClick = () => {
-        navigate("/signup");
-    };
-
+function SignupPage() {
     return (
         <div css={s.layout}>
-            <div css={s.loginBox}>
-                <div css={s.title}>로그인</div>
+            <div css={s.signinBox}>
+                <div css={s.title}>회원가입</div>
                 <div css={s.inputBox}>
                     <div>
                         <div>이메일 주소</div>
                         <input type="text" placeholder="이메일" />
                     </div>
                     <div>
+                        <div>닉네임</div>
+                        <input type="text" placeholder="닉네임" />
+                    </div>
+                    <div>
                         <div>패스워드</div>
                         <input type="password" placeholder="비밀번호" />
                     </div>
-                    <div onClick={handleSignupClick}>회원가입</div>
+                    <div>
+                        <div>패스워드 확인</div>
+                        <input type="password" placeholder="비밀번호 확인" />
+                    </div>
                 </div>
                 <div css={s.buttonBox}>
-                    <button>로그인</button>
+                    <button>회원가입</button>
                 </div>
             </div>
         </div>
     );
 }
 
-export default SigninPage;
+export default SignupPage;
--- src/routes/AuthRoute.jsx
+++ src/routes/AuthRoute.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import SigninPage from "../pages/authPages/signinPage/SigninPage";
+import SignupPage from "../pages/authPages/signupPage/SignupPage";
 
 function AuthRoute() {
     return (
         <>
             <Routes>
                 <Route path="/signin" element={<SigninPage />} />
+                <Route path="/signup" element={<SignupPage />} />
             </Routes>
         </>
     );
