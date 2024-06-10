fdf5c366d18cdad709cec3dcfddc03e81e88cf14
dongyoon7212 <dongyoon7212@naver.com>
2024-06-11 00:52:04 +0900

    Feat: 회원가입 유효성 검사 완료

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
+import { REGEX } from "../constants/regex";
 
-export const useInput = () => {
+export const useInput = (property) => {
     const [value, setValue] = useState("");
     const [message, setMessage] = useState(null);
 
@@ -8,6 +9,26 @@ export const useInput = () => {
         if (!value) {
             setMessage(() => null);
             return;
+        }
+        const regexEntries = Object.entries(REGEX);
+        for (let [k, v] of regexEntries) {
+            if (property === k) {
+                if (v.regexr.test(value)) {
+                    setMessage(() => {
+                        return {
+                            type: "success",
+                            text: "",
+                        };
+                    });
+                } else {
+                    setMessage(() => {
+                        return {
+                            type: "error",
+                            text: v.text,
+                        };
+                    });
+                }
+            }
         }
     }, [value]);
 
