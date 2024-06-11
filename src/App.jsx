40498fff73e76e00715c1dc7e02f4aff99878d9e
dongyoon7212 <dongyoon7212@naver.com>
2024-06-11 14:49:37 +0900

    Feat: 로그인 로직 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { useQuery } from "react-query";
 import Header from "./components/MainPage/Header/Header";
 import PageLayout from "./components/PageLayout/PageLayout";
 import AuthRoute from "./routes/AuthRoute";
 import MainRoute from "./routes/MainRoute";
+import { getPricipalRequest } from "./apis/auth/authApi";
 
 function App() {
+    const principalQuery = useQuery(["principalQuery"], getPricipalRequest, {
+        retry: 0,
+        refetchOnWindowFocus: false,
+        onSuccess: (response) => {
+            console.log("onSuccess");
+            console.log(response);
+        },
+        onError: (error) => {
+            console.log("오류");
+            console.log(error);
+        },
+    });
+    
     return (
         <>
             <PageLayout>
