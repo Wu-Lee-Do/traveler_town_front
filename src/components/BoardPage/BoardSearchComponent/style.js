5f362430e012192d6fa9950eaa2c9e6273d75b75
seowooNote <dkr0210@naver.com>
2024-06-23 18:20:32 +0900

    Refactor: 맛집 검색 컴포넌트화 완료, Feat: 검색에 따른 게시글 리스트 조회 기능 구현

---
+++ src/components/BoardPage/BoardSearchComponent/style.js
@@ -0,0 +1,18 @@
+import { css } from "@emotion/react";
+
+export const searchInput = css`
+    width: 693px;
+    height: 19px;
+    border: none;
+    outline: none;
+    margin-left: 15px;
+    font-size: 16px;
+`;
+
+export const searchButton = css`
+    padding-right: 10px;
+    border: none;
+    background-color: transparent;
+    font-size: 20px;
+    cursor: pointer;
+`;
