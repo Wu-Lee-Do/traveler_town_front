94fe3816c2293ee6896b6e1cad74f35df2a9d10d
seowooNote <dkr0210@naver.com>
2024-06-24 14:46:58 +0900

    Feat: 맛집 게시글 작성 페이지 layout 구현

--- src/pages/boardPages/mustGoRestaurantWritePage/MustGoRestaurantWritePage.jsx
+++ src/pages/boardPages/mustGoRestaurantWritePage/MustGoRestaurantWritePage.jsx
@@ -3,7 +3,21 @@ import * as s from "./style";
 
 function MustGoRestaurantWritePage() {
   return (
-    <div css={s.layout}>MustGoRestaurantWritePage</div>
+    <div css={s.layout}>
+      <div css={s.box}>
+        <div css={s.titleBox}>
+          <h1>맛집 게시글 작성</h1>
+        </div>
+        <div css={s.contentBox}>
+          <div>
+            글쓰기 영역
+          </div>
+          <div>
+            주전부리
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/boardPages/mustGoRestaurantWritePage/style.js
+++ src/pages/boardPages/mustGoRestaurantWritePage/style.js
@@ -1,5 +1,37 @@
 import { css } from "@emotion/react";
 
 export const layout = css `
+    width: 100%;
+    height: auto;
     padding-top: 68px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const box = css`
+    width: 1136px;
+    height: auto;
+
+    & > h1 {
+        cursor: default;
+    }
+`;
+
+export const titleBox = css`
+    width: 100%;
+`;
+
+export const contentBox = css`
+    display: flex;
+    width: 100%;
+
+    & > div:nth-of-type(1) {
+        width: 70%;
+    }
+
+    & > div:nth-of-type(2) {
+        width: 30%;
+    }
 `;
