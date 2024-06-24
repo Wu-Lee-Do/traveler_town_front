77ba60e3de819fd06460c4c06bb628a4beca96fe
dongyoon7212 <dongyoon7212@naver.com>
2024-06-25 01:02:10 +0900

    Feat: HOTFIX: ROUTE 수정

--- src/routes/BoardRoute.jsx
+++ src/routes/BoardRoute.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-<<<<<<< 82-맛집-게시글-작성-페이지-구현
 import MustGoRestaurantListPage from "../pages/boardPages/mustGoRestaurantListPage/MustGoRestaurantListPage";
-=======
-import MustGoRestaurantPage from "../pages/boardPages/mustGoRestaurantListPage/MustGoRestaurantPage";
->>>>>>> main
+
 import BookmarkPage from "../pages/bookmarkPage/BookmarkPage";
 import MustGoRestaurantWritePage from "../pages/boardPages/mustGoRestaurantWritePage/MustGoRestaurantWritePage";
 
