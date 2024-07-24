f47a8bda7bec72364388d04800584d5a7ac2322a
dongyoon7212 <dongyoon7212@naver.com>
2024-07-24 23:44:32 +0900

    Feat: 서버 주소 변경

--- src/constants/serverAddress.js
+++ src/constants/serverAddress.js
@@ -1,4 +1,5 @@
-const LOCAL_ADDRESS = "http://localhost:8080";
+const LOCAL_ADDRESS =
+    "http://ec2-3-36-22-124.ap-northeast-2.compute.amazonaws.com:8080";
 
 export default function getServerAddress() {
     return LOCAL_ADDRESS;
